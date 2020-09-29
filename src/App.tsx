8fbdbf0ec81a25fc940360a0d3393cafb402939f
Paavan Bhavsar <pbhavsar@kyruus.com>
2020-09-29 01:02:19 -0400

    Basic app with PoC button that plays a note

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useState, useMemo} from 'react'
+import * as Tone from 'tone'
+
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
+  const [started, setStarted] = useState(false)
+  const synth = useMemo(() => new Tone.Synth().toDestination(), [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +16,22 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!started ? (
+          <button onClick={() => setStarted(true)}>Start Synth</button>
+        ) : (
+          <button
+            onMouseDown={() => {
+              const now = Tone.now()
+              synth.triggerAttack("C4", now)
+            }}
+            onMouseUp={() => {
+              const now = Tone.now()
+              synth.triggerRelease(now)
+            }}>Play Note</button>
+        )}
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
