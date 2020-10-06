d82eeb9157fa617535d0696cde53f5e26c40a336
Paavan Bhavsar <pbhavsar@kyruus.com>
2020-10-06 01:22:03 -0400

    Add ScaledRangeInput component to simplify nonlinear slider controls

---
+++ src/ScaledRangeInput.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ScaleContinuousNumeric } from 'd3-scale'
+
+interface ScaledRangeInputProps extends React.HTMLProps<HTMLInputElement> {
+  scale: ScaleContinuousNumeric<number, number>
+  onUpdate: (newValue: number) => void
+  value: number
+}
+
+export default function ScaledRangeInput(
+  props: ScaledRangeInputProps
+): JSX.Element {
+  const { scale, value, onUpdate, ...rest } = props
+
+  return (
+    <input
+      {...rest}
+      type="range"
+      value={scale.invert(value)}
+      onChange={(evt) => {
+        const newValue = scale(parseFloat(evt.target.value))
+        if (newValue) onUpdate(newValue)
+      }}
+    />
+  )
+}
